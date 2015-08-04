444b57ef339d01fca0582e700c05d8e19806a66e
lavandenberg <sandervdberg@yahoo.com>
2015-08-04 13:26:05 +0800

    Initial Commit

---
+++ untitled folder/trader/trader/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  trader
+//
+//  Created by Sander van den Berg on 4/8/15.
+//  Copyright (c) 2015 Sander van den Berg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
